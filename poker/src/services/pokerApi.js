17831ff48c897d89153829665b969e6405c69620
Claudio Rojas Rodriguez <claudio.dcv@gmail.com>
2017-10-05 08:22:15 -0300

    todo function comprobate

--- poker/src/services/pokerApi.js
+++ poker/src/services/pokerApi.js
@@ -1,25 +1,82 @@
 import axios from 'axios';
 
 const obtainToken = 'https://services.comparaonline.com/dealer/deck';
-const obtainCards = (token) => `https://services.comparaonline.com/dealer/deck/${token}/deal/5`;
+const obtainCards = (token, n) => `https://services.comparaonline.com/dealer/deck/${token}/deal/${n}`;
 
-
-const getCards = cb => {
+const getToken = () => localStorage.getItem('token')
+const setToken = token => localStorage.setItem('token', token)
+// 405 si no hay cartas suficientes
+// 404 si no existe baraja o el token expiro, reiniciar juego
+export const newToken = () => {
   axios.post(obtainToken)
   .then((response) => {
-    axios.get(obtainCards(response.data))
+    console.log(`init new token ${response.data}`);
+    setToken(response.data)
+  }).catch((error) => {
+    newToken()
+  });
+}
+
+const getApiToken = cb => {
+  axios.post(obtainToken)
+  .then((response) => {
+    console.log(`new token ${response.data}`);
+    setToken(response.data)
+    cb()
+  }).catch((error) => {
+    getApiToken(cb)
+  });
+}
+
+export const getCards = cb => {
+  // si existe token se hace consulta a cartas
+  if (getToken()) {
+    axios.get(obtainCards(getToken(), 5))
     .then((response) => {
       cb(response)
     })
     .catch((error) => {
-      getCards(cb);
+      if (!error.response) {
+        getCards(cb)
+      } else {
+        console.log(error.response.status);
+        switch (error.response.status) {
+          case 500:
+              getCards(cb)
+            break;
+          case 405:
+              getApiToken(() => cb(405))
+            break;
+          case 404:
+              getApiToken(() => cb(404))
+            break;
+          default:
+        }
+      }
     });
-  })
-  .catch((error) => {
-    getCards(cb);
-  });
-};
-
-export {
-  getCards,
-};
+  }
+}
+//
+// const getCards = cb => {
+//   axios.post(obtainToken)
+//   .then((response) => {
+//     axios.get(obtainCards(response.data))
+//     .then((response) => {
+//       cb(response)
+//     })
+//     .catch((error) => {
+//       debugger
+//       console.log(error.response.status);
+//       getCards(cb);
+//     });
+//   })
+//   .catch((error) => {
+//     debugger
+//     console.log(error.response.status);
+//     getCards(cb);
+//   });
+// };
+//
+// export {
+//   getCards,
+// };
